53d9975dda0bfb55edfb6df41e4449e5c215778c
Arnaud Dole <dole.arnaud@gmail.com>
2020-05-12 21:06:24 +0400

    add kubectl template

---
+++ kubernetes/README.md
@@ -0,0 +1,5 @@
+# Kubernetes client (kubectl)
+
+Here simple `kubectl` client.
+
+For full cluster, see https://github.com/ardole/k8s-cluster
---
+++ kubernetes/install-kubectl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# Install Kubectl
+sudo bash -c 'cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF'
+
+sudo yum install -y kubectl-1.17.2 --disableexcludes=kubernetes
